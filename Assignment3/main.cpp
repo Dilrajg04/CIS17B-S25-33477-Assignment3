823a80f8ecf2eb6d80cc998dc9a11c4268a56d51
Dilrajg04 <dgrewal2004@gmail.com>
2025-03-24 16:23:54 -0700

    Initial Commit

---
+++ Assignment3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Assignment3
+//
+//  Created by Dilraj Grewal on 3/24/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
